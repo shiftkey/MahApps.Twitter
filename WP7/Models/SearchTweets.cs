e43b3e8b85b42f5db6a837298d791c7262f5fdab
aeoth <paul@theleagueofpaul.com>
2010-11-06 18:45:09 +1100

    Search, baby, search.

--- WP7/Models/SearchTweets.cs
+++ WP7/Models/SearchTweets.cs
@@ -12,19 +12,17 @@ namespace MahApps.Twitter.Models
         [JsonProperty(PropertyName = "to_user", NullValueHandling = NullValueHandling.Ignore)]
         public String InReplyToScreenName { get; set; }
         
-        public String Source { get; set; }
-
         [JsonProperty(PropertyName = "from_user")]
         public String ContactName { get; set; }
+
+        [JsonProperty(PropertyName = "profile_image_url")]
+        public String ContactImage { get; set; }
 
         [DefaultValue(0)]
         [JsonProperty(PropertyName = "to_user_id", NullValueHandling = NullValueHandling.Ignore)]
         public long? InReplyToUserId { get; set; }
 
         public long? Id { get; set; }
-
-        [JsonProperty("created_at")]
-        public object CreatedDate { get; set;}
 
         public DateTime Created { get { return CreatedDate.ToString().ParseDateTime(); } }
     }
