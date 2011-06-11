31b928f849c0102e922f8df5ae1cba6211cbca7b
shiftkey <me@brendanforster.com>
2011-06-11 12:38:40 +0200

    added getters to interface

--- WP7/ITwitterClient.cs
+++ WP7/ITwitterClient.cs
@@ -1,9 +1,17 @@
 ﻿using MahApps.RESTBase;
+using MahApps.Twitter.Methods;
 
 namespace MahApps.Twitter
 {
     public interface ITwitterClient : IRestClientBase
     {
-
+        Account Account { get; }
+        Statuses Statuses { get; }
+        Block Block { get; }
+        List Lists { get; }
+        Search Search { get; }
+        DirectMessages DirectMessages { get; }
+        Favourites Favourites { get; }
+        Friendship Friendships { get; }
     }
 }
