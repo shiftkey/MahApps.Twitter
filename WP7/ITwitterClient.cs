87828970c942f5460e7d10d10cdc363c489937a9
shiftkey <me@brendanforster.com>
2011-06-23 01:56:55 +0200

    updated linked files
    --HG--
    rename : NET40/IBaseTwitterClient.cs => WP7/IBaseTwitterClient.cs

--- WP7/ITwitterClient.cs
+++ WP7/ITwitterClient.cs
@@ -2,29 +2,9 @@
 using System.Collections.Generic;
 using System.Net;
 using MahApps.RESTBase;
-using MahApps.Twitter.Methods;
-using MahApps.Twitter.Models;
 
 namespace MahApps.Twitter
 {
-    public interface IBaseTwitterClient : IRestClientBase
-    {
-        ITwitterResponse Deserialise<T>(string content) where T : ITwitterResponse;
-        bool Encode { get; set; }
-
-        Account Account { get; }
-        Statuses Statuses { get; }
-        Block Block { get; }
-        List Lists { get; }
-        Search Search { get; }
-        FriendsAndFollowers FriendsAndFollowers { get; set; }
-        DirectMessages DirectMessages { get; }
-        Favourites Favourites { get; }
-
-        Friendship Friendships { get; }
-        Users Users { get; }
-    }
-
     public interface ITwitterClient : IBaseTwitterClient
     {
         WebRequest DelegatedRequest(String Url, TwitterClient.Format format);
