0c8135185f093b819c5fdd1307aa3104709e01c0
shiftkey <me@brendanforster.com>
2011-06-23 01:57:56 +0200

    cleanup interface method

--- WP7/ITwitterClient.cs
+++ WP7/ITwitterClient.cs
@@ -7,7 +7,7 @@ namespace MahApps.Twitter
 {
     public interface ITwitterClient : IBaseTwitterClient
     {
-        WebRequest DelegatedRequest(String Url, TwitterClient.Format format);
+        WebRequest DelegatedRequest(string url, TwitterClient.Format format);
         event TwitterClient.VoidDelegate StreamingReconnectAttemptEvent;
         event TwitterClient.VoidDelegate StreamingDisconnectedEvent;
         IAsyncResult BeginStream(TwitterClient.TweetCallback callback, List<string> tracks);
