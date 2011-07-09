296df7be2547260bbd0da238556c1fb5cd42216b
shiftkey <me@brendanforster.com>
2011-07-09 12:20:29 +1000

    added tests for in_reply_to_status_id and lat/long parameters

--- Tests/NET40/Methods/StatusesTests.cs
+++ Tests/NET40/Methods/StatusesTests.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Hammock;
+using Hammock.Web;
 using MahApps.Twitter.Delegates;
 using MahApps.Twitter.Methods;
 using MahApps.Twitter.Models;
 using NSubstitute;
+using NSubstitute.Core;
 using NUnit.Framework;
 
 namespace MahApps.Twitter.NET40.UnitTests.Methods
@@ -174,6 +177,62 @@ namespace MahApps.Twitter.NET40.UnitTests.Methods
             statuses.BeginUpdate("some words go here", endBeginUpdate);
 
             Assert.That(wasCalled, CallbackDidNotFire);
+        }
+
+        [Test]
+        public void BeginUpdate_WithInReplyToIdSet_PopulatesParameter()
+        {
+            // act
+            const string inReplyToId = "12345";
+
+            var twitterClient = Substitute.For<IBaseTwitterClient>();
+            twitterClient.When(a => a.BeginRequest(Arg.Any<string>(), Arg.Any<IDictionary<string, string>>(), Arg.Any<WebMethod>(), Arg.Any<RestCallback>()))
+             .Do(c => AssertParameter(c, "in_reply_to_status_id", inReplyToId));
+            var statuses = new Statuses(twitterClient);
+
+            // assert
+            GenericResponseDelegate endBeginUpdate = (a, b, c) => { };
+
+            // act
+            statuses.BeginUpdate("some words go here", "12345", endBeginUpdate);
+        }
+
+        [Test]
+        public void BeginUpdate_WithLocationSet_PopulatesParameters()
+        {
+            // act
+            const double latitute = 10.0;
+            const double longitude = 15.0;
+
+            var twitterClient = Substitute.For<IBaseTwitterClient>();
+            twitterClient.When(a => a.BeginRequest(Arg.Any<string>(), Arg.Any<IDictionary<string, string>>(), Arg.Any<WebMethod>(), Arg.Any<RestCallback>()))
+             .Do(c =>
+             {
+                 AssertParameter(c, "lat", latitute);
+                 AssertParameter(c, "long", longitude);
+             });
+            var statuses = new Statuses(twitterClient);
+
+            // assert
+            GenericResponseDelegate endBeginUpdate = (a, b, c) => { };
+
+            // act
+            statuses.BeginUpdate("some words go here", "12345", latitute, longitude, endBeginUpdate);
+        }
+
+        private static void AssertParameter(CallInfo c, string key, object value)
+        {
+            var parameters = c.Args()[1] as IDictionary<string, string>;
+            if (!parameters.ContainsKey(key))
+            {
+                Assert.Fail("Expected key '{0}' but was not found", "in_reply_to_status_id");
+            }
+
+            var actual = parameters[key];
+            if (actual != value.ToString())
+            {
+                Assert.Fail("Expected value '{0}' but got '{1}'", value, actual);
+            }
         }
     }
 }
