967ea69ea90f71156ab91cb4c1c5179ac9190ab0
shiftkey <me@brendanforster.com>
2011-07-09 11:50:54 +1000

    added tests for model classes

---
+++ Tests/NET40/Models/GeoTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using MahApps.Twitter.Models;
+using NUnit.Framework;
+
+namespace MahApps.Twitter.NET40.UnitTests.Models
+{
+    [TestFixture]
+    public class GeoTests
+    {
+        [Test]
+        public void Long_WhenNotSet_ReturnsNull()
+        {
+            var geo = new Geo();
+            geo.Coordinates = null;
+
+            Assert.That(geo.Long, Is.Null);
+        }
+
+        [Test]
+        public void Lat_WhenNotSet_ReturnsNull()
+        {
+            var geo = new Geo();
+            geo.Coordinates = null;
+
+            Assert.That(geo.Lat, Is.Null);
+        }
+
+        [Test]
+        public void Long_WhenSet_ReturnsValue()
+        {
+            var geo = new Geo();
+            geo.Coordinates = new[] { 10.0, 15.0 };
+
+            Assert.That(geo.Long == 15.0);
+        }
+
+        [Test]
+        public void Lat_WhenSet_ReturnsValue()
+        {
+            var geo = new Geo();
+            geo.Coordinates = new[] {10.0, 15.0};
+
+            Assert.That(geo.Lat == 10.0);
+        }
+
+        [Test]
+        public void Lat_WhenOneSet_ReturnsValue()
+        {
+            var geo = new Geo();
+            geo.Coordinates = new[] {10.0 };
+
+            Assert.That(geo.Lat == 10.0);
+        }
+
+        [Test]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void Long_WhenOneSet_ThrowsException()
+        {
+            var geo = new Geo();
+            geo.Coordinates = new[] { 10.0 };
+
+            Assert.That(geo.Long == 10.0);
+        }
+    }
+}
---
+++ Tests/NET40/Models/ModelsTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MahApps.Twitter.Models;
+using NUnit.Framework;
+
+namespace MahApps.Twitter.NET40.UnitTests.Models
+{
+    [TestFixture]
+    public class ModelsTests
+    {
+        [Test]
+        public void Created_ForDirectMessageUsingTestTime_IsPopulated()
+        {
+            // arrange
+            var directMessage = new DirectMessage();
+            directMessage.CreatedDate = "Sun Mar 18 06:42:26 +0000 2007";
+
+            // act
+            var dateTime = directMessage.Created;
+
+            Assert.That(dateTime.DayOfWeek == DayOfWeek.Sunday);
+            Assert.That(dateTime.Year == 2007);
+        }
+
+        [Test]
+        public void Created_ForTweetUsingTestTime_IsPopulated()
+        {
+            // arrange
+            var directMessage = new Tweet();
+            directMessage.CreatedDate = "Sun Mar 18 06:42:26 +0000 2007";
+
+            // act
+            var dateTime = directMessage.Created;
+
+            Assert.That(dateTime.DayOfWeek == DayOfWeek.Sunday);
+            Assert.That(dateTime.Year == 2007);
+        }
+
+        [Test]
+        public void Created_ForSearchTweetUsingTestTime_IsPopulated()
+        {
+            // arrange
+            var directMessage = new SearchTweet();
+            directMessage.CreatedDate = "Sun Mar 18 06:42:26 +0000 2007";
+
+            // act
+            var dateTime = directMessage.Created;
+
+            Assert.That(dateTime.DayOfWeek == DayOfWeek.Sunday);
+            Assert.That(dateTime.Year == 2007);
+        }
+
+        [Test]
+        public void Created_ForUserUsingTestTime_IsPopulated()
+        {
+            // arrange
+            var directMessage = new User();
+            directMessage.CreatedDate = "Sun Mar 18 06:42:26 +0000 2007";
+
+            // act
+            var dateTime = directMessage.Created;
+
+            Assert.That(dateTime.DayOfWeek == DayOfWeek.Sunday);
+            Assert.That(dateTime.Year == 2007);
+        }
+    }
+}
