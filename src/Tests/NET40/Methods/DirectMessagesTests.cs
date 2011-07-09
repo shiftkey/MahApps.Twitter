18c181343db9563b610b28319b53764127c00f73
shiftkey <me@brendanforster.com>
2011-07-10 08:35:58 +1000

    added tests for directmessages/send

--- src/Tests/NET40/Methods/DirectMessagesTests.cs
+++ src/Tests/NET40/Methods/DirectMessagesTests.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using Hammock;
 using Hammock.Web;
 using MahApps.Twitter.Delegates;
@@ -49,10 +46,50 @@ namespace MahApps.Twitter.Tests.Methods
                  c.AssertParameter("trim_user", false);
                  c.AssertParameter("include_entities", false);
              });
-            var statuses = new Statuses(twitterClient);
+            var statuses = new DirectMessages(twitterClient);
 
             // act
-            statuses.BeginMentions((a, b, c) => { });
+            statuses.BeginDirectMessages((a, b, c) => { });
+        }
+
+        [Test]
+        public void BeginSentDirectMessages_WithValidUserName_ReturnsValidUser()
+        {
+            var wasCalled = false;
+            var twitterClient = Substitute.For<IBaseTwitterClient>();
+            twitterClient.SetReponseBasedOnRequestPath();
+            var directMessages = new DirectMessages(twitterClient);
+
+            // assert
+            GenericResponseDelegate endCreate = (a, b, c) =>
+            {
+                wasCalled = true;
+                var results = c as IEnumerable<DirectMessage>;
+                Assert.That(results, Is.Not.Null);
+                Assert.That(results, Is.Not.Empty);
+            };
+
+            // act
+            directMessages.BeginSentDirectMessages(endCreate);
+
+            Assert.That(wasCalled, Errors.CallbackDidNotFire);
+        }
+
+        [Test]
+        public void BeginSentDirectMessages_WithDefaultScenarios_SetsParameter()
+        {
+            // act
+            var twitterClient = Substitute.For<IBaseTwitterClient>();
+            twitterClient.When(a => a.BeginRequest(Arg.Any<string>(), Arg.Any<IDictionary<string, string>>(), Arg.Any<WebMethod>(), Arg.Any<RestCallback>()))
+             .Do(c =>
+             {
+                 c.AssertParameter("trim_user", false);
+                 c.AssertParameter("include_entities", false);
+             });
+            var statuses = new DirectMessages(twitterClient);
+
+            // act
+            statuses.BeginSentDirectMessages((a, b, c) => { });
         }
     }
 }
