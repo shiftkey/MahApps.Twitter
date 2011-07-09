9c3b2104808dc539b3e17cac53e8de935573a0b0
shiftkey <me@brendanforster.com>
2011-07-10 08:15:06 +1000

    added tests for friendships/destroy

--- src/Tests/NET40/Methods/FriendshipTests.cs
+++ src/Tests/NET40/Methods/FriendshipTests.cs
@@ -1,4 +1,7 @@
-﻿using MahApps.Twitter.Delegates;
+﻿using System.Collections.Generic;
+using Hammock;
+using Hammock.Web;
+using MahApps.Twitter.Delegates;
 using MahApps.Twitter.Methods;
 using MahApps.Twitter.Models;
 using NSubstitute;
@@ -18,7 +21,7 @@ namespace MahApps.Twitter.Tests.Methods
             var friendships = new Friendship(twitterClient);
 
             // assert
-            GenericResponseDelegate endSearch = (a, b, c) =>
+            GenericResponseDelegate endCreate = (a, b, c) =>
             {
                 wasCalled = true;
                 var results = c as User;
@@ -26,9 +29,61 @@ namespace MahApps.Twitter.Tests.Methods
             };
 
             // act
-            friendships.BeginCreate("abcde", endSearch);
+            friendships.BeginCreate("abcde", endCreate);
 
             Assert.That(wasCalled, Errors.CallbackDidNotFire);
+        }
+
+        [Test]
+        public void BeginCreate_WithValidUser_SetsParameter()
+        {
+            const string username = "abcde";
+            var twitterClient = Substitute.For<IBaseTwitterClient>();
+            twitterClient.When(a => a.BeginRequest(Arg.Any<string>(), Arg.Any<IDictionary<string, string>>(), Arg.Any<WebMethod>(), Arg.Any<RestCallback>()))
+                         .Do(c => c.AssertParameter("screen_name", username));
+            var friendships = new Friendship(twitterClient);
+
+            GenericResponseDelegate endSearch = (a, b, c) => { };
+
+            // act
+            friendships.BeginCreate(username, endSearch);
+        }
+
+        [Test]
+        public void BeginDestroy_WithValidUserName_ReturnsValidUser()
+        {
+            var wasCalled = false;
+            var twitterClient = Substitute.For<IBaseTwitterClient>();
+            twitterClient.SetReponseBasedOnRequestPath();
+            var friendships = new Friendship(twitterClient);
+
+            // assert
+            GenericResponseDelegate endCreate = (a, b, c) =>
+            {
+                wasCalled = true;
+                var results = c as User;
+                Assert.That(results, Is.Not.Null);
+            };
+
+            // act
+            friendships.BeginDestroy("abcde", endCreate);
+
+            Assert.That(wasCalled, Errors.CallbackDidNotFire);
+        }
+
+        [Test]
+        public void BeginDestroy_WithValidUser_SetsParameter()
+        {
+            const string username = "abcde";
+            var twitterClient = Substitute.For<IBaseTwitterClient>();
+            twitterClient.When(a => a.BeginRequest(Arg.Any<string>(), Arg.Any<IDictionary<string, string>>(), Arg.Any<WebMethod>(), Arg.Any<RestCallback>()))
+                         .Do(c => c.AssertParameter("screen_name", username));
+            var friendships = new Friendship(twitterClient);
+
+            GenericResponseDelegate endSearch = (a, b, c) => { };
+
+            // act
+            friendships.BeginDestroy(username, endSearch);
         }
     }
 }
