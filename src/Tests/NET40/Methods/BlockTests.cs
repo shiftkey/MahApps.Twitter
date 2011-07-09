0d8873955720c921537e5f70812c0f815fecb40c
shiftkey <me@brendanforster.com>
2011-07-09 19:49:11 +1000

    started on tests for Block

---
+++ src/Tests/NET40/Methods/BlockTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace MahApps.Twitter.NET40.UnitTests.Methods
+{
+    [TestFixture]
+    public class BlockTests
+    {
+        [Test]
+        public void BeginBlock_WithSomeName_ReturnsUser()
+        {
+            Assert.Inconclusive();
+        }
+
+        [Test]
+        public void BeginBlock_WithSomeName_ProvidesParameter()
+        {
+            Assert.Inconclusive();
+        }
+
+        [Test]
+        public void BeginSpamBlock_WithSomeName_ReturnsUser()
+        {
+            Assert.Inconclusive();
+        }
+
+        [Test]
+        public void BeginSpamBlock_WithSomeName_ProvidesParameter()
+        {
+            Assert.Inconclusive();
+        }
+    }
+}
