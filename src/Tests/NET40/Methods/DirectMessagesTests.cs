bd356071909d17247a758ea08f233dacf1bb7c83
shiftkey <me@brendanforster.com>
2011-07-10 08:30:49 +1000

    started tests for directmessages

---
+++ src/Tests/NET40/Methods/DirectMessagesTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Hammock;
+using Hammock.Web;
+using MahApps.Twitter.Delegates;
+using MahApps.Twitter.Methods;
+using MahApps.Twitter.Models;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace MahApps.Twitter.Tests.Methods
+{
+    [TestFixture]
+    public class DirectMessagesTests
+    {
+        [Test]
+        public void BeginDirectMessages_WithValidUserName_ReturnsValidUser()
+        {
+            var wasCalled = false;
+            var twitterClient = Substitute.For<IBaseTwitterClient>();
+            twitterClient.SetReponseBasedOnRequestPath();
+            var directMessages = new DirectMessages(twitterClient);
+
+            // assert
+            GenericResponseDelegate endCreate = (a, b, c) =>
+            {
+                wasCalled = true;
+                var results = c as IEnumerable<DirectMessage>;
+                Assert.That(results, Is.Not.Null);
+                Assert.That(results, Is.Not.Empty);
+            };
+
+            // act
+            directMessages.BeginDirectMessages(endCreate);
+
+            Assert.That(wasCalled, Errors.CallbackDidNotFire);
+        }
+
+        [Test]
+        public void BeginDirectMessages_WithDefaultScenarios_SetsParameter()
+        {
+            // act
+            var twitterClient = Substitute.For<IBaseTwitterClient>();
+            twitterClient.When(a => a.BeginRequest(Arg.Any<string>(), Arg.Any<IDictionary<string, string>>(), Arg.Any<WebMethod>(), Arg.Any<RestCallback>()))
+             .Do(c =>
+             {
+                 c.AssertParameter("trim_user", false);
+                 c.AssertParameter("include_entities", false);
+             });
+            var statuses = new Statuses(twitterClient);
+
+            // act
+            statuses.BeginMentions((a, b, c) => { });
+        }
+    }
+}
