66de270a96fcdf1d0e5af067f1440123309f698f
shiftkey <me@brendanforster.com>
2011-07-10 08:10:57 +1000

    added first test

---
+++ src/Tests/NET40/Methods/FriendshipTests.cs
@@ -0,0 +1,34 @@
+﻿using MahApps.Twitter.Delegates;
+using MahApps.Twitter.Methods;
+using MahApps.Twitter.Models;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace MahApps.Twitter.Tests.Methods
+{
+    [TestFixture]
+    public class FriendshipTests
+    {
+        [Test]
+        public void BeginCreate_WithValidUserName_ReturnsValidUser()
+        {
+            var wasCalled = false;
+            var twitterClient = Substitute.For<IBaseTwitterClient>();
+            twitterClient.SetReponseBasedOnRequestPath();
+            var friendships = new Friendship(twitterClient);
+
+            // assert
+            GenericResponseDelegate endSearch = (a, b, c) =>
+            {
+                wasCalled = true;
+                var results = c as User;
+                Assert.That(results, Is.Not.Null);
+            };
+
+            // act
+            friendships.BeginCreate("abcde", endSearch);
+
+            Assert.That(wasCalled, Errors.CallbackDidNotFire);
+        }
+    }
+}
