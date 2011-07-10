78869018ce50a9980690f2c534ae88392b3f931d
shiftkey <me@brendanforster.com>
2011-07-10 10:52:31 +1000

    a couple more tests

--- src/Tests/NET40/TwitterClientTests.cs
+++ src/Tests/NET40/TwitterClientTests.cs
@@ -26,6 +26,26 @@ namespace MahApps.Twitter.Tests
         }
 
         [Test]
+        public void Constructor_WithMockService_IsSetToTwittersAPI()
+        {
+            // arrange
+            var restClient = Substitute.For<IRestClient>();
+            const string consumerKey = "foo";
+            const string consumerSecret = "bar";
+
+            // act
+            var client = new TwitterClient(restClient, consumerKey, consumerSecret, "");
+
+            //assert
+            Assert.That(client.OAuthBase == "https://api.twitter.com/oauth/");
+            Assert.That(client.TokenRequestUrl == "request_token");
+            Assert.That(client.TokenAuthUrl == "authorize");
+            Assert.That(client.TokenAccessUrl == "access_token");
+            Assert.That(client.Authority == "https://api.twitter.com/");
+            Assert.That(client.Version == "1");
+        }
+
+        [Test]
         public void Constructor_WithCallback_IsSetToCredentials()
         {
             // arrange
@@ -38,6 +58,23 @@ namespace MahApps.Twitter.Tests
             // assert
             var credentials = client.Credentials as OAuthCredentials;
             Assert.That(credentials.CallbackUrl == callback);
+        }
+
+        [Test]
+        public void DelegatedRequest_WithParameters_SetsHeaders()
+        {
+            Assert.Inconclusive();
+        }
+
+        [Test]
+        public void DelegatedRequest_WithParameters_SetsCorrectPath()
+        {
+            Assert.Inconclusive();
+        }
+
+        public void BeginXAuthAuthenticate_WithParameters_CallsToTwitter()
+        {
+            Assert.Inconclusive();
         }
     }
 }
