bf1747e11a2f47159497a04cf50b3d48c826f985
shiftkey <me@brendanforster.com>
2011-07-21 21:01:57 +1000

    some more tests for favourites

--- src/Tests/NET40/Methods/FavouritesTests.cs
+++ src/Tests/NET40/Methods/FavouritesTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Hammock;
 using Hammock.Web;
 using MahApps.Twitter.Delegates;
@@ -39,19 +40,40 @@ namespace MahApps.Twitter.Tests.Methods
         public void BeginCreate_ForAllScenarios_SetsParameter()
         {
             // arrange
-            var screenName = "abcde";
-            var text = "defgh";
+            var wasReceived = false;
+            var twitterClient = Substitute.For<IBaseTwitterClient>();
+            twitterClient.When(a => a.BeginRequest(Arg.Any<string>(), Arg.Any<IDictionary<string, string>>(), Arg.Any<WebMethod>(), Arg.Any<RestCallback>()))
+                         .Do(c =>
+                                 {
+                                     wasReceived = true;
+                                     var url = c.Args().First().ToString();
+                                     Assert.That(url.EndsWith("1234.json"));
+                                 });
+            var statuses = new Favourites(twitterClient);
+
+            // act
+            statuses.BeginCreate("1234", (a, b, c) => { });
+            Assert.That(wasReceived);
+        }
+
+        [Test]
+        public void BeginDestroy_ForAllScenarios_SetsParameter()
+        {
+            // arrange
+            var wasReceived = false;
             var twitterClient = Substitute.For<IBaseTwitterClient>();
             twitterClient.When(a => a.BeginRequest(Arg.Any<string>(), Arg.Any<IDictionary<string, string>>(), Arg.Any<WebMethod>(), Arg.Any<RestCallback>()))
                          .Do(c =>
                          {
-                             c.AssertParameter("screen_name", screenName);
-                             c.AssertParameter("text", text);
+                             wasReceived = true;
+                             var url = c.Args().First().ToString();
+                             Assert.That(url.EndsWith("1234.json"));
                          });
             var statuses = new Favourites(twitterClient);
 
             // act
-            //statuses.BeginCreate(screenName, text, (a, b, c) => { });
+            statuses.BeginDestroy("1234", (a, b, c) => { });
+            Assert.That(wasReceived);
         }
     }
 }
